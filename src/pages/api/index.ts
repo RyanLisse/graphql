be0f4c9dcbad0be052f6784afdef05b303f8da6b
RyanLisse <ryan.lisse@gmail.com>
2022-07-10 13:20:46 +0200

    connect docker with Mysql db

---
+++ src/pages/api/index.ts
@@ -0,0 +1,28 @@
+import { createServer } from "@graphql-yoga/node";
+import { join } from "path";
+import { readFileSync } from "fs";
+import { Resolvers } from "types";
+
+const typeDefs = readFileSync(join(process.cwd(), "schema.graphql"), {
+  encoding: "utf-8",
+});
+
+const resolvers: Resolvers = {
+  Query: {
+    cart: (_, { id }) => {
+      return {
+        id,
+        totalItems: 0,
+      };
+    },
+  },
+};
+const server = createServer({
+  endpoint: "/api",
+  schema: {
+    typeDefs,
+    resolvers,
+  },
+});
+
+export default server.requestListener;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <h1 className="text-xl text-purple-500">Hello world</h1>;
+};
+
+export default Home;
